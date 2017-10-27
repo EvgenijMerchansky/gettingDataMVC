0e6cf801afa86ff1f592ecd04055aa8a13e20a5b
Evgenij Merchansky <evgenij.merchansky@gmail.com>
2017-10-27 17:02:16 +0300

    second refact

--- gettingDataMVC/webpack.config.js
+++ gettingDataMVC/webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
 
     devServer: {
         contentBase: path.join(__dirname, 'public'),
-        hot: true
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -20,5 +19,9 @@ module.exports = {
             template: './index.html'
         }),
     ],
+    target: 'node',
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".js", ".node"]
+    },
     devtool: 'cheap-eval-source-map'
-}
+};
